9a4d78d0b7cb819e812f38cb999582d0bf74659b
PAVAN VARSHNEY <91428581+Pavanvarshney@users.noreply.github.com>
2021-12-17 02:08:51 +0530

    Add files via upload

---
+++ array descending order in bubble sort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int size,i,j,temp;
+    scanf("%d",&size);
+    int x[size];
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        for(j=i+1;j<size;j++)
+        {
+            if(x[i]<x[j])
+            {
+                temp=x[i];
+                x[i]=x[j];
+                x[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<size;i++)
+    {
+        printf("%d\n",x[i]);
+    }
+    return 0;
+}
+
+
